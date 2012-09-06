cf04be949cca61f45be0a93779d825388a9ded95
Stefan Kaes <stefan.kaes@xing.com>
2012-09-06 11:08:28 +0200

    évent drivent test_publisher

--- test_publisher.c
+++ test_publisher.c
@@ -21,6 +21,30 @@ void log_zmq_error(int rc)
   }
 }
 
+int timer_event(zloop_t *loop, zmq_pollitem_t *item, void *pusher)
+{
+  int rc;
+  zmsg_t *message = zmsg_new();
+  zmsg_addstr(message, "testtesttest");
+  zmsg_addstr(message, "data");
+  rc = zmsg_send(&message, pusher);
+  if (!zctx_interrupted) {
+    assert(rc==0);
+  }
+  return 0;
+}
+
+int forward(zloop_t *loop, zmq_pollitem_t *item, void *publisher)
+{
+  int rc;
+  zmsg_t *message = zmsg_recv(item->socket);
+
+  rc = zmsg_send(&message, publisher);
+  assert(rc == 0);
+  return 0;
+}
+
+
 int main(int argc, char const * const *argv)
 {
   int rc;
@@ -30,30 +54,52 @@ int main(int argc, char const * const *argv)
   zctx_set_hwm(context, 1);
   zctx_set_linger(context, 100);
 
-  void *socket = zsocket_new(context, ZMQ_PUB);
-  assert(socket);
+  void *pusher = zsocket_new(context, ZMQ_PUSH);
+  assert(pusher);
+  zsocket_set_hwm(pusher, 1000);
+  zsocket_set_linger(pusher, 500);
+  rc = zsocket_connect(pusher, "tcp://localhost:12345");
+  assert(rc==0);
 
-  zsocket_set_hwm(socket, 1000);
-  zsocket_set_linger(socket, 500);
-  zsocket_set_reconnect_ivl(socket, 100); // 100 ms
-  zsocket_set_reconnect_ivl_max(socket, 10 * 1000); // 10 s
+  void *puller = zsocket_new(context, ZMQ_PULL);
+  assert(puller);
+  zsocket_set_hwm(puller, 1000);
+  zsocket_set_linger(puller, 500);
+  rc = zsocket_bind(puller, "tcp://*:12345");
+  if (rc != 12345){
+    printf("bind failed: %s\n", zmq_strerror(errno));
+  }
+  assert(rc == 12345);
 
-  rc = zsocket_bind(socket, "tcp://*:12346");
+  void *publisher = zsocket_new(context, ZMQ_PUB);
+  assert(publisher);
+  zsocket_set_hwm(publisher, 1000);
+  zsocket_set_linger(publisher, 500);
+  rc = zsocket_bind(publisher, "tcp://*:12346");
   assert(rc==12346);
 
-  zmsg_t *msg = NULL;
+  // set up event loop
+  zloop_t *loop = zloop_new();
+  assert(loop);
+  zloop_set_verbose(loop, 0);
 
-  while (!zctx_interrupted) {
-    zmsg_t *message = zmsg_new();
-    zmsg_addstr(message, "testtesttest");
-    zmsg_addstr(message, "data");
-    rc = zmsg_send(&message, socket);
-    if (!zctx_interrupted) {
-      assert(rc==0);
-    }
-  }
+  // push data every 10 ms
+  rc = zloop_timer(loop, 1, 0, timer_event, pusher);
+  assert(rc == 0);
 
-  zsocket_destroy(context, socket);
+  zmq_pollitem_t item;
+  item.socket = puller;
+  item.events = ZMQ_POLLIN;
+  rc = zloop_poller(loop, &item, forward, publisher);
+  assert(rc == 0);
+
+  rc = zloop_start(loop);
+  // printf("zloop return: %d", rc);
+
+  zloop_destroy(&loop);
+  assert(loop == NULL);
+
+  // cleanup
   zctx_destroy(&context);
 
   return 0;
