13a296608dc3e300cb40f0bfd2dcded5cdfd3c74
Stefan Kaes <stefan.kaes@xing.com>
2014-02-09 07:25:40 +0100

    added config file for proxy

--- logjam-proxy.c
+++ logjam-proxy.c
@@ -23,7 +23,7 @@ void log_zmq_error(int rc)
 
 static zhash_t *subscriptions = NULL;
 
-void* sub_socket_new(zctx_t *context, const char* end_point)
+void* sub_socket_new(zctx_t *context)
 {
     int rc;
     void *socket = zsocket_new(context, ZMQ_SUB);
@@ -32,8 +32,6 @@ void* sub_socket_new(zctx_t *context, const char* end_point)
     zsocket_set_linger(socket, 500);
     zsocket_set_reconnect_ivl(socket, 100); // 100 ms
     zsocket_set_reconnect_ivl_max(socket, 10 * 1000); // 10 s
-    rc = zsocket_connect(socket, end_point);
-    assert(rc == 0);
     return socket;
 }
 
@@ -44,26 +42,28 @@ typedef struct {
     size_t messages_dropped;
 } subscription_t;
 
-void* worker_socket_new(zctx_t *context, char* file_name)
+void* worker_socket_new(zctx_t *context, char* file_name, char* ipcdir)
 {
     int rc;
     void *socket = zsocket_new(context, ZMQ_PUSH);
     assert(socket);
-    zsocket_set_sndhwm(socket, 1000);
-    zsocket_set_linger(socket, 500);
-    rc = zsocket_connect(socket, "ipc://tmp/%s", file_name);
+    zsocket_set_sndhwm(socket, 5000);
+    zsocket_set_linger(socket, 100);
+    assert(ipcdir);
+    assert(*ipcdir == '/');
+    rc = zsocket_connect(socket, "ipc:/%s/%s", ipcdir, file_name);
     assert(rc == 0);
     return socket;
 }
 
-void add_subscription(zctx_t *context, void* sub_socket, char* stream)
+void add_subscription(zctx_t *context, void* sub_socket, char* stream, char* ipcdir)
 {
     subscription_t *subscription = malloc(sizeof(subscription_t));
     assert(subscription);
     subscription->stream = stream;
     subscription->messages_dropped = 0;
     subscription->messages_transmitted = 0;
-    subscription->socket = worker_socket_new(context, stream);
+    subscription->socket = worker_socket_new(context, stream, ipcdir);
     assert(subscription->socket);
     zsocket_set_subscribe(sub_socket, stream);
     zhash_update(subscriptions, stream, subscription);
@@ -124,10 +124,51 @@ void get_subscription_and_forward_message(zmsg_t* msg)
     }
 }
 
+void* configure(zconfig_t *config, zctx_t *context)
+{
+    int rc;
+    zconfig_t *current;
+
+    void* sub_socket = sub_socket_new(context);
+
+    zconfig_t *endpoints = zconfig_locate(config, "frontend/endpoints");
+    current = zconfig_child(endpoints);
+    assert(current);
+    do {
+        char *binding = zconfig_value(current);
+        rc = zsocket_connect(sub_socket, binding);
+        assert(rc == 0);
+        current = zconfig_next(current);
+    } while (current);
+
+    char *ipcdir = zconfig_resolve(config, "ipcdir", "/tmp");
+    zconfig_t *streams = zconfig_locate(config, "backend/streams");
+    current = zconfig_child(streams);
+    assert(current);
+    do {
+        char *stream = zconfig_name(current);
+        add_subscription(context, sub_socket, stream, ipcdir);
+        current = zconfig_next(current);
+    } while (current);
+
+    return sub_socket;
+}
+
 int main(int argc, char const * const *argv)
 {
-    char *subscription_endpoint = "tcp://localhost:12346";
-    char *test_stream =  "request-stream-test-development";
+    const char *config_file = "logjam.conf";
+    if (argc > 2) {
+        fprintf(stderr, "usage: %s [config-file]\n", argv[0]);
+        exit(0);
+    }
+    if (argc == 2) {
+        config_file = argv[1];
+    }
+    if (!zsys_file_exists(config_file)) {
+        fprintf(stderr, "missing config file: %s\n", config_file);
+        exit(0);
+    }
+    zconfig_t* config = zconfig_load((char*)config_file);
 
     setvbuf(stdout,NULL,_IOLBF,0);
     setvbuf(stderr,NULL,_IOLBF,0);
@@ -138,11 +179,10 @@ int main(int argc, char const * const *argv)
     zctx_set_linger(context, 100);
 
     subscriptions = zhash_new();
-    void *socket = sub_socket_new(context, subscription_endpoint);
-    add_subscription(context, socket, test_stream);
+    void *sub_socket = configure(config, context);
 
     while (1) {
-        zmsg_t *msg = zmsg_recv(socket);
+        zmsg_t *msg = zmsg_recv(sub_socket);
         if (zctx_interrupted)
             break;
 
@@ -157,7 +197,7 @@ int main(int argc, char const * const *argv)
 
     zhash_foreach(subscriptions, dump_subscription, NULL);
 
-    zsocket_destroy(context, socket);
+    zsocket_destroy(context, sub_socket);
     zctx_destroy(&context);
 
     return 0;
