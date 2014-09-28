4308247b3ff96e946c2a7d371d8f1302ce09fb01
Stefan Kaes <stefan.kaes@xing.com>
2014-09-28 12:16:38 +0200

    note on percentiles

--- src/logjam-importer.c
+++ src/logjam-importer.c
@@ -2130,6 +2130,7 @@ void processor_add_frontend_data(processor_state_t *self, parser_state_t *pstate
     request_data.minute = processor_setup_minute(self, request);
     request_data.total_time = processor_setup_time(self, request, "page_time");
 
+    // TODO: revisit when switching to percentiles
     if (request_data.total_time > 300000) {
         fprintf(stderr, "[W] dropped request data with nonsensical page_time\n");
         dump_json_object(stderr, request);
@@ -2173,6 +2174,7 @@ void processor_add_ajax_data(processor_state_t *self, parser_state_t *pstate, js
     request_data.minute = processor_setup_minute(self, request);
     request_data.total_time = processor_setup_time(self, request, "ajax_time");
 
+    // TODO: revisit when switching to percentiles
     if (request_data.total_time > 60000) {
         fprintf(stderr, "[W] dropped request data with nonsensical ajax_time\n");
         dump_json_object(stderr, request);
