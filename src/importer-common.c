0b33ffd0edd35099c70b3817b2dae2b8d6675240
Stefan Kaes <stefan.kaes@xing.com>
2016-08-09 10:34:27 +0200

    importer: removed debug output for date switching
    Just adding a new application causes the app problems on the
    current tokumx servers. A new setup with mongodb 3.2.8 and
    wired tiger storage engine seems to fix the problems. In all
    likelihood, the importer code isn't the problem.

--- src/importer-common.c
+++ src/importer-common.c
@@ -294,10 +294,10 @@ bool config_update_date_info()
 
     bool changed = strcmp(old_date, iso_date_today);
 
-    printf("[D] date info: old        ISO date is %s\n", old_date);
-    printf("[D] date info: today's    ISO date is %s\n", iso_date_today);
-    printf("[D] date info: tomorrow's ISO date is %s\n", iso_date_tomorrow);
-    printf("[D] date info: changed             is %d\n", changed);
+    // printf("[D] date info: old        ISO date is %s\n", old_date);
+    // printf("[D] date info: today's    ISO date is %s\n", iso_date_today);
+    // printf("[D] date info: tomorrow's ISO date is %s\n", iso_date_tomorrow);
+    // printf("[D] date info: changed             is %d\n", changed);
 
     return changed;
 }
