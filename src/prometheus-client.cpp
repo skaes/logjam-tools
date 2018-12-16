78a27937f8f17ca8465529c69785687eb01eb526
Stefan Kaes <stefan.kaes@xing.com>
2018-12-16 13:08:47 +0100

    renamed variables

--- src/prometheus-client.cpp
+++ src/prometheus-client.cpp
@@ -1,100 +1,100 @@
-#include <prometheus-client.h>
 #include <prometheus/exposer.h>
+#include "prometheus-client.h"
 #include <prometheus/registry.h>
 
 static struct prometheus_client_t {
     prometheus::Exposer *exposer;
     std::shared_ptr<prometheus::Registry> registry;
-    prometheus::Family<prometheus::Counter> *update_count_family;
-    prometheus::Family<prometheus::Counter> *update_time_family;
-    prometheus::Counter *update_count;
-    prometheus::Counter *update_time;
-    prometheus::Family<prometheus::Counter> *insert_count_family;
-    prometheus::Family<prometheus::Counter> *insert_time_family;
-    prometheus::Counter *insert_count;
-    prometheus::Counter *insert_time;
-    prometheus::Family<prometheus::Counter> *received_count_family;
-    prometheus::Counter *received_count;
-    prometheus::Family<prometheus::Counter> *missed_count_family;
-    prometheus::Counter *missed_count;
-    prometheus::Family<prometheus::Counter> *parsed_count_family;
-    prometheus::Counter *parsed_count;
-    prometheus::Family<prometheus::Gauge> *queued_updates_count_family;
-    prometheus::Gauge *queued_updates_count;
-    prometheus::Family<prometheus::Gauge> *queued_inserts_count_family;
-    prometheus::Gauge *queued_inserts_count;
+    prometheus::Family<prometheus::Counter> *updates_total_family;
+    prometheus::Counter *updates_total;
+    prometheus::Family<prometheus::Counter> *updates_seconds_family;
+    prometheus::Counter *updates_seconds;
+    prometheus::Family<prometheus::Counter> *inserts_total_family;
+    prometheus::Counter *inserts_total;
+    prometheus::Family<prometheus::Counter> *inserts_seconds_family;
+    prometheus::Counter *inserts_seconds;
+    prometheus::Family<prometheus::Counter> *received_msgs_total_family;
+    prometheus::Counter *received_msgs_total;
+    prometheus::Family<prometheus::Counter> *missed_msgs_total_family;
+    prometheus::Counter *missed_msgs_total;
+    prometheus::Family<prometheus::Counter> *parsed_msgs_total_family;
+    prometheus::Counter *parsed_msgs_total;
+    prometheus::Family<prometheus::Gauge> *queued_updates_family;
+    prometheus::Gauge *queued_updates;
+    prometheus::Family<prometheus::Gauge> *queued_inserts_family;
+    prometheus::Gauge *queued_inserts;
 } client;
 
 void prometheus_client_init(const char* address)
 {
-    // create a http server running on port 8080
+    // create a http server running on the given address
     client.exposer = new prometheus::Exposer{address};
-    // create a metrics registry with component=main labels applied to
-    // all its metrics
+    // create a metrics registry
     client.registry = std::make_shared<prometheus::Registry>();
 
-    client.update_count_family = &prometheus::BuildCounter()
-        .Name("importer_updates_count")
-        .Help("How many updates is this server processing")
+    // declare metrics families, counters and gauges
+    client.updates_total_family = &prometheus::BuildCounter()
+        .Name("importer_updates_total")
+        .Help("How many updates has this importer performed")
         .Register(*client.registry);
 
-    client.update_count = &client.update_count_family->Add({});
+    client.updates_total = &client.updates_total_family->Add({});
 
-    client.update_time_family = &prometheus::BuildCounter()
-        .Name("importer_updates_time")
-        .Help("How many seconds is this server running?")
+    client.updates_seconds_family = &prometheus::BuildCounter()
+        .Name("importer_updates_seconds")
+        .Help("How many seconds has this importer spent updating")
         .Register(*client.registry);
 
-    client.update_time = &client.update_time_family->Add({});
+    client.updates_seconds = &client.updates_seconds_family->Add({});
 
-    client.insert_count_family = &prometheus::BuildCounter()
-        .Name("importer_inserts_count")
-        .Help("How many inserts is this server processing")
+    client.inserts_total_family = &prometheus::BuildCounter()
+        .Name("importer_inserts_total")
+        .Help("How many inserts has this importer performed")
         .Register(*client.registry);
 
-    client.insert_count = &client.insert_count_family->Add({});
+    client.inserts_total = &client.inserts_total_family->Add({});
 
-    client.insert_time_family = &prometheus::BuildCounter()
-        .Name("importer_inserts_time")
-        .Help("How many seconds is this server running?")
+    client.inserts_seconds_family = &prometheus::BuildCounter()
+        .Name("importer_inserts_seconds")
+        .Help("How many seconds has this imorter spent inserting")
         .Register(*client.registry);
 
-    client.insert_time = &client.insert_time_family->Add({});
+    client.inserts_seconds = &client.inserts_seconds_family->Add({});
 
-    client.received_count_family = &prometheus::BuildCounter()
-        .Name("importer_msgs_received_count")
-        .Help("How many receiveds is this server processing")
+    client.received_msgs_total_family = &prometheus::BuildCounter()
+        .Name("importer_msgs_received_total")
+        .Help("How many logjam messages has this importer received")
         .Register(*client.registry);
 
-    client.received_count = &client.received_count_family->Add({});
+    client.received_msgs_total = &client.received_msgs_total_family->Add({});
 
-    client.missed_count_family = &prometheus::BuildCounter()
-        .Name("importer_msgs_missed_count")
-        .Help("How many log messages where missed on the bus")
+    client.missed_msgs_total_family = &prometheus::BuildCounter()
+        .Name("importer_msgs_missed_total")
+        .Help("How many logjam messages where missed by this importer")
         .Register(*client.registry);
 
-    client.missed_count = &client.missed_count_family->Add({});
+    client.missed_msgs_total = &client.missed_msgs_total_family->Add({});
 
-    client.parsed_count_family = &prometheus::BuildCounter()
-        .Name("importer_msgs_parsed_count")
-        .Help("How many messages where parsed by the importer")
+    client.parsed_msgs_total_family = &prometheus::BuildCounter()
+        .Name("importer_msgs_parsed_total")
+        .Help("How many logjam messages where parsed by this importer")
         .Register(*client.registry);
 
-    client.parsed_count = &client.parsed_count_family->Add({});
+    client.parsed_msgs_total = &client.parsed_msgs_total_family->Add({});
 
-    client.queued_updates_count_family = &prometheus::BuildGauge()
-        .Name("importer_updates_queued_count")
-        .Help("How many database updates are currently queued")
+    client.queued_updates_family = &prometheus::BuildGauge()
+        .Name("importer_updates_queued")
+        .Help("How many database updates are currently waiting to be processed by the importer")
         .Register(*client.registry);
 
-    client.queued_updates_count = &client.queued_updates_count_family->Add({});
+    client.queued_updates = &client.queued_updates_family->Add({});
 
-    client.queued_inserts_count_family = &prometheus::BuildGauge()
-        .Name("importer_inserts_queued_count")
-        .Help("How many database inserts are currently queued")
+    client.queued_inserts_family = &prometheus::BuildGauge()
+        .Name("importer_inserts_queued")
+        .Help("How many database inserts are currently waiting to be processed by the importer")
         .Register(*client.registry);
 
-    client.queued_inserts_count = &client.queued_inserts_count_family->Add({});
+    client.queued_inserts = &client.queued_inserts_family->Add({});
 
     // ask the exposer to scrape the registry on incoming scrapes
     // client.exposer->RegisterCollectable(client.registry);
@@ -110,15 +110,15 @@ void prometheus_client_count(int metric, double value)
 {
     switch (metric) {
     case IMPORTER_UPDATE_COUNT:
-        client.update_count->Increment(value);
+        client.updates_total->Increment(value);
     case IMPORTER_INSERT_COUNT:
-        client.insert_count->Increment(value);
+        client.inserts_total->Increment(value);
     case IMPORTER_MSGS_RECEIVED_COUNT:
-        client.received_count->Increment(value);
+        client.received_msgs_total->Increment(value);
     case IMPORTER_MSGS_MISSED_COUNT:
-        client.missed_count->Increment(value);
+        client.missed_msgs_total->Increment(value);
     case IMPORTER_MSGS_PARSED_COUNT:
-        client.parsed_count->Increment(value);
+        client.parsed_msgs_total->Increment(value);
     }
 }
 
@@ -126,9 +126,9 @@ void prometheus_client_gauge(int metric, double value)
 {
     switch (metric) {
     case IMPORTER_QUEUED_UPDATES_COUNT:
-        client.queued_updates_count->Increment(value);
+        client.queued_updates->Increment(value);
     case IMPORTER_QUEUED_INSERTS_COUNT:
-        client.queued_inserts_count->Increment(value);
+        client.queued_inserts->Increment(value);
     }
 }
 
@@ -136,8 +136,8 @@ void prometheus_client_timing(int metric, double value)
 {
     switch (metric) {
     case IMPORTER_UPDATE_TIME:
-        client.update_time->Increment(value);
+        client.updates_seconds->Increment(value);
     case IMPORTER_INSERT_TIME:
-        client.insert_time->Increment(value);
+        client.inserts_seconds->Increment(value);
     }
 }
