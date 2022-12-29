0d128906fed5cce7cd5546c16104147acb654109
Stefan Kaes <stefan.kaes@xing.com>
2022-12-29 18:46:24 +0100

    drop xenial, add jammy

--- build_tools.rb
+++ build_tools.rb
@@ -19,7 +19,7 @@ vendor "skaes@railsexpress.de"
 
 files "#{prefix}/bin/logjam-*"
 
-depends "logjam-libs#{suffix}", ">= 0.8-0"
+depends "logjam-libs#{suffix}", ">= 0.10-1"
 
 apt_setup "apt-get update -y && apt-get install apt-transport-https ca-certificates -y"
 apt_setup "echo 'deb [trusted=yes] https://railsexpress.de/packages/ubuntu/#{codename} ./' >> /etc/apt/sources.list"
